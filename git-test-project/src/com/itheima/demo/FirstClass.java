62fceb3b4907c6137e09efb77e4cccbde013a650
zhu <975242695@qq.com>
2019-03-20 21:45:52 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/demo/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.demo;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
+
+    }
+}
