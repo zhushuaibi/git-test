33c412acbabf852c60c61eee2116249d3376ccaa
zhu <975242695@qq.com>
2019-03-20 22:06:03 +0800

    添加注释和算法

--- git-test-project/src/com/itheima/demo/FirstClass.java
+++ git-test-project/src/com/itheima/demo/FirstClass.java
@@ -7,10 +7,12 @@ public class FirstClass {
         System.out.println("hello");
         System.out.println("hello");
 
-
+        //你们好啊   这是我加的注释
     }
 
     public void test(){
         System.out.println("haha");
+
+        int i = 1 + 1;
     }
 }
